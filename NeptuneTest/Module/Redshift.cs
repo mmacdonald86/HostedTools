5f5b6bbb2b4fe9af178cd5b7c8aa9272e3593f30
mmacdonald86 <michael.macdonald@groundtruth.com>
2021-07-28 12:01:22 -0400

    Add Redshift module

---
+++ NeptuneTest/Module/Redshift.cs
@@ -0,0 +1,31 @@
+﻿
+
+using com.antlersoft.HostedTools.Framework.Interface.Setting;
+using com.antlersoft.HostedTools.Interface;
+using com.antlersoft.HostedTools.Sql.Model;
+using com.gt.NeptuneTest.Interface;
+
+namespace com.gt.NeptuneTest.Module
+{
+    public class Redshift : PoiDb
+    {
+        public override string Name => "Redshift";
+
+        public async override System.Threading.Tasks.Task<IHtValue> Setup(ITestInstance config, IHtValue moduleSettings)
+        {
+            var result = await SetupSkeletonDatabase(config, moduleSettings);
+
+            string dockerHost = TestSetup.PoiDbNetworkAddress.Value<string>(SettingManager);
+
+            // If not UseDocker, we must be *in* docker, so need host to load archive to to be docker poidb
+            string host = TestSetup.UseDocker.Value<bool>(SettingManager) ? "localhost" : dockerHost;
+
+            var connection = new PostgreSqlConnectionSource($"Host={host};Port=5432;Username=admin;Password=admin;Database={result[DB_NAME].AsString}");
+
+            LoadArchive(connection, config.Monitor, GetArchiveFolder(config, moduleSettings), config.ExpandValue(moduleSettings[RepoConfig].AsString), config.ExpandValue(moduleSettings[ArchiveTitle].AsString));
+
+            config.SetConfigurationValue("REDSHIFT_HOST", $"\"{dockerHost}\"");
+            return result;
+        }
+    }
+}
