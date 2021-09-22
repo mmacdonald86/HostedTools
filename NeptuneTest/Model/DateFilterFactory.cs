914ad6f845b08657f93f8e4804e541634389f773
mmacdonald86 <michael.macdonald@groundtruth.com>
2021-09-22 19:23:17 -0400

    Add DateFilterFactory to NeptuneTest

---
+++ NeptuneTest/Model/DateFilterFactory.cs
@@ -0,0 +1,38 @@
+﻿using com.antlersoft.HostedTools.Archive.Interface;
+using com.antlersoft.HostedTools.Archive.Model;
+using com.antlersoft.HostedTools.Framework.Model;
+using com.antlersoft.HostedTools.Interface;
+using System;
+using System.ComponentModel.Composition;
+
+namespace com.gt.NeptuneTest.Model
+{
+    [Export(typeof(IArchiveFilterFactory))]
+    public class DateFilterFactory : HostedObjectBase, IArchiveFilterFactory
+    {
+        public string Name => "DateFilter";
+
+        public IArchiveFilter ConfigureFilter(IHtValue configuration)
+        {
+            var baseDate = configuration["BaseDate"];
+            var result = new DateArchiveFilter();
+            result.Offset = TimeSpan.FromTicks(0L);
+            if (baseDate != null)
+            {
+                if (baseDate.IsDictionary)
+                {
+                    result.Offset = DateTime.UtcNow - DateTime.FromFileTimeUtc(baseDate["Ticks"].AsLong);
+                }
+                else
+                {
+                    DateTime parsed;
+                    if (DateTime.TryParse(baseDate.AsString, null, System.Globalization.DateTimeStyles.AssumeUniversal, out parsed))
+                    {
+                        result.Offset = DateTime.UtcNow - parsed;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
